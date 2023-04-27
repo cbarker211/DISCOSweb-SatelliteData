45c724340b0018048648e8745f9db57c7d984d99
Connor Barker <connor.barker@ucl.ac.uk>
2023-04-27 14:35:35 +0100

    Adding gitignore file, wait function, token file, fixed payload function.

--- DISCOSweb.py
+++ DISCOSweb.py
@@ -3,14 +3,16 @@ import requests
 from datetime import datetime, timedelta
 import time
 import sys
+import numpy as np
 
-""" Script to request data from the DISCOSweb database.
-NB: Ech request is limited to 30 results, and there is a maximum limit of requests in a certain timeframe (unknown).
-To solve this, the script makes multiple requests over different filters."""
+""" Script to request data from the DISCOSweb database. 
+NB: Each request is limited to 30 results, and there is a maximum limit of requests in a certain timeframe (20 requests per 60s).
+To solve this, the script makes multiple requests over different filters, and automatically calculates the time required to wait."""
 
 #Define the URL for the data, and the authorisation token (get a new token here if needed https://discosweb.esoc.esa.int/tokens).
 URL = 'https://discosweb.esoc.esa.int'
-token = 'IjNiYzUyZjNkLThmNzEtNDVkZC1hMTliLWNiNDgxODkyZjVmOCI.0yAE_9jpGAvf2lCadU85mWWMKkE'
+token = np.loadtxt("token.txt", dtype=str)
+filename = 'output.txt'
 
 def server_request(user_params):
     response = requests.get(
@@ -23,7 +25,7 @@ def server_request(user_params):
             },
             #Use the user-specified parameters.
             params=user_params
-        )    
+        ) 
     return response
 
 def server_request_objects(user_params,id):
@@ -37,8 +39,13 @@ def server_request_objects(user_params,id):
             },
             #Use the user-specified parameters.
             params=user_params
-        )    
+        )
     return response
+
+def wait_function(message,wait_time):
+    for i in range(wait_time,0,-1):
+        print(f"{message}Too many requests, retrying in {str(i)}s.", end="\r", flush=True)
+        time.sleep(1)
  
 def get_yearly_launches(year): 
     full_launch_list = []
@@ -57,9 +64,9 @@ def get_yearly_launches(year):
             if response.ok:
                 break
             else:
-                for i in range(60,0,-1):
-                    print(f"Too many requests, retrying in {str(i)}s.", end="\r", flush=True)
-                    time.sleep(1)
+                message = ""
+                wait_time=(int(response.headers["X-Ratelimit-Reset"])-int(time.time()))+1
+                wait_function(message,wait_time)
                 continue
         doc = response.json()
         temp_launch_list = doc['data']
@@ -87,35 +94,50 @@ def launches_per_year(start_year,end_year):
                 launch_success+=1
             else:
                 print("Error")         
-        print(f'In {year}, there were {launch_success} successful launches and {launch_failure} unsuccessful launches.')
+        print(f'\nIn {year}, there were {launch_success} successful launches and {launch_failure} unsuccessful launches.')
         
 def payloads_launched_per_year(start_year,end_year):
     for year in range(start_year,end_year+1):
         full_launch_list = get_yearly_launches(year)
         num_payloads = 0
         for launch in full_launch_list:
+            full_object_list = []
             #First filter only the successful launches.
             if launch["attributes"]["failure"] == False:
                 #Need to get the object information for each specific launch, and then sum the payloads.
-                params={          
+                params={
+                        'filter' : "eq(objectClass,Payload)",
+                        'sort' : 'id'         
                     }
                 while True:
                     response = server_request_objects(params,launch["id"])
                     if response.ok:
+                        doc = response.json()
+                        temp_object_list = doc['data']
+                        full_object_list.extend(temp_object_list)
+                        if len(temp_object_list) > 0:
+                            final_object_id = str(int(temp_object_list[-1]["id"]) + 1)
+                        if len(temp_object_list) == 30:
+                            params={ 
+                                    'filter' : f"eq(objectClass,Payload)&ge(id,{final_object_id})",
+                                    'sort' : 'id'         
+                                }
+                            continue
                         break
-                    else:
-                        for i in range(60,0,-1):
-                            print(f"Too many requests, retrying in {str(i)}s.", end="\r", flush=True)
-                            time.sleep(1)
+                    elif response.status_code == 400:
+                        print("Client Error")
+                        break
+                    elif response.status_code == 429:
+                        message = f"Currently found {num_payloads} payloads. "
+                        wait_time=(int(response.headers["X-Ratelimit-Reset"])-int(time.time()))+1
+                        wait_function(message,wait_time)
                         continue
-                doc = response.json()
-                object_list = doc['data']
-                for launch_object in object_list:
-                    if launch_object["attributes"]["objectClass"] == "Payload":
-                        num_payloads += 1
-        print(f'In {year}, there were {num_payloads} payloads successfully launched.')        
+                #print(launch["id"],len(full_object_list))
+                num_payloads += len(full_object_list)
+        with open(filename, 'a') as f:
+            f.write(f'In {year}, there were {num_payloads} payloads successfully launched.\n')       
      
-start_year = 2017
-end_year = 2023 
+start_year = 2010
+end_year = 2023
 #launches_per_year(start_year,end_year)
 payloads_launched_per_year(start_year,end_year)
